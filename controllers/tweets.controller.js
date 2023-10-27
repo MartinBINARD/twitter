d0218cf56e2106108894227fa0781aa07d59b96e
MartinBINARD <martin.binard.dev@gmail.com>
2023-10-27 17:37:09 +0200

    Feature : Add new route and controller for deleteTweet + create list rerender method after delete

--- controllers/tweets.controller.js
+++ controllers/tweets.controller.js
@@ -1,10 +1,14 @@
-const { getTweets, createTweet } = require("../queries/tweets.queries");
+const {
+  getTweets,
+  createTweet,
+  deleteTweet,
+} = require("../queries/tweets.queries");
 
 exports.tweetList = async (req, res, next) => {
   try {
     // exec nodejs child_process Transform query result into promise
     const tweets = await getTweets();
-    res.render("tweets/tweet-list", { tweets });
+    res.render("tweets/tweet", { tweets });
   } catch (error) {
     next(e);
   }
@@ -23,5 +27,20 @@ exports.tweetCreate = async (req, res, next) => {
   } catch (e) {
     const errors = Object.keys(e.errors).map((key) => e.errors[key].message);
     res.status(400).render("tweets/tweet-form", { errors });
+  }
+};
+
+exports.tweetDelete = async (req, res, next) => {
+  try {
+    const tweetId = req.params.tweetId;
+
+    await deleteTweet(tweetId);
+
+    /* Render only the tweet list after delete component
+    to avoid rendering the entire page */
+    const tweets = await getTweets();
+    res.render("tweets/tweet-list", { tweets });
+  } catch (error) {
+    next(e);
   }
 };
--- routes/tweets.js
+++ routes/tweets.js
@@ -3,12 +3,16 @@ const {
   tweetList,
   tweetNew,
   tweetCreate,
+  tweetDelete,
 } = require("../controllers/tweets.controller");
 
+// GET LIST
 router.get("/", tweetList);
-
+// GET FORM
 router.get("/new", tweetNew);
-
+// POST FORM
 router.post("/", tweetCreate);
+// DELETE TWEET
+router.delete("/:tweetId", tweetDelete);
 
 module.exports = router;
