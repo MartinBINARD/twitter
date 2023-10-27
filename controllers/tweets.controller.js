13070fb3b75c013f514c59e4b34bab2fd1748f6e
MartinBINARD <martin.binard.dev@gmail.com>
2023-10-27 16:42:54 +0200

    Refactor: Create tweets.queries.js for tweets.controller.js logic

--- controllers/tweets.controller.js
+++ controllers/tweets.controller.js
@@ -1,9 +1,9 @@
-const Tweet = require("../database/models/tweet.model");
+const { getTweets, createTweet } = require("../queries/tweets.queries");
 
 exports.tweetList = async (req, res, next) => {
   try {
     // exec nodejs child_process Transform query result into promise
-    const tweets = await Tweet.find({}).exec();
+    const tweets = await getTweets();
     res.render("tweets/tweet-list", { tweets });
   } catch (error) {
     next(e);
@@ -18,9 +18,8 @@ exports.tweetCreate = async (req, res, next) => {
   try {
     const body = req.body;
     // import Mongoose models to transform user form into Mongoose document
-    const newTweet = new Tweet(body);
-    await newTweet.save();
-    res.redirect("/");
+    await createTweet(body);
+    res.redirect("/tweets");
   } catch (e) {
     const errors = Object.keys(e.errors).map((key) => e.errors[key].message);
     res.status(400).render("tweets/tweet-form", { errors });
---
+++ queries/tweets.queries.js
@@ -0,0 +1,13 @@
+const Tweet = require("../database/models/tweet.model");
+
+// queries for controller logic : GET CREATE
+
+exports.getTweets = () => {
+  return Tweet.find({}).exec();
+};
+
+exports.createTweet = (tweet) => {
+  const newTweet = new Tweet(tweet);
+
+  return newTweet.save();
+};
