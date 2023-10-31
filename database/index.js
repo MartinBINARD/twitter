7b16c77c9d9e1de2bd4becd8d65106361a6d02c8
MartinBINARD <martin.binard.dev@gmail.com>
2023-10-31 17:40:53 +0100

    Fix : MongoStore connection

--- database/index.js
+++ database/index.js
@@ -3,12 +3,17 @@ require("dotenv").config();
 // mongoose library use async method to connect to data base with user database credentials
 const mongoose = require("mongoose");
 
-mongoose
+exports.clientPromise = mongoose
   .connect(
     `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.yxs0hv5.mongodb.net/twitter?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
     }
   )
-  .then(() => console.log("connexion db ok !"))
+  .then((client) => {
+    console.log("connexion db ok !");
+    // Pass client and return it for clientPromise resolution in MongoStore
+
+    return client;
+  })
   .catch((err) => console.log(err));
