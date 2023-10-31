64b2ca6f346fd05bdfefc884e708af1e54c549c2
MartinBINARD <martin.binard.dev@gmail.com>
2023-10-31 23:44:45 +0100

    Feature guard : Add avatar key to user model

--- database/models/user.model.js
+++ database/models/user.model.js
@@ -9,6 +9,7 @@ const userSchema = schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
+  avatar: { type: String, default: "/images/default-profile.svg" },
 });
 
 // hash password during schema creation and before model creation
