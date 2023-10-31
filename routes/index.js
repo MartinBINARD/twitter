dcf49471f767e5ab7af21d9a21ae0f2439839ee8
MartinBINARD <martin.binard.dev@gmail.com>
2023-10-31 23:33:26 +0100

    Feature guard : Create isAuthenticated guard on tweets routes + Add unique value on user  name & email

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
+const { ensureAuthenticated } = require("../config/guards.config");
 const tweets = require("./tweets.routes");
 const users = require("./users.routes");
 const auth = require("./auth.routes");
 
 // Connect  directly to tweets router to interact with
-router.use("/tweets", tweets);
+// use ensureAuthenticated to protect tweets routes
+router.use("/tweets", ensureAuthenticated, tweets);
 router.use("/users", users);
 router.use("/auth", auth);
 // redirect default routes to tweet
