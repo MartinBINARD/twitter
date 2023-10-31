d8bd149ea3c3b44de91a515015a0862c5a52a8da
MartinBINARD <martin.binard.dev@gmail.com>
2023-10-31 14:00:31 +0100

    Feature user-register : Create users.routers and users.model and rename tweet.routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
-const tweets = require("./tweets");
+const tweets = require("./tweets.routes");
+const user = require("./users.routes");
 
 // Connect  directly to tweets router to interact with
 router.use("/tweets", tweets);
+router.use("/users", users);
 // redirect default routes to tweet
 router.get("/", (req, res) => {
   res.redirect("/tweets");
