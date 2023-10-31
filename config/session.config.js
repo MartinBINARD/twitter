e371b10754e54f32093f49abdf8a855fde003416
MartinBINARD <martin.binard.dev@gmail.com>
2023-10-31 14:10:44 +0100

    Feature user-register : Add clientPromise in MongoStore

--- config/session.config.js
+++ config/session.config.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const { app } = require("../app");
 const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo");
+const { clientPromise } = require("../database");
 
+/*
+    Forced to not save session in in MongoStore if session is not modified.
+    saveUninitialized is set to false to not keep empty session.
+    httpOnly to set the cookie accessible or not for the client
+    Created an AUTH_AGE variable to maintain cookies as long as session is avialable
+    Added clientPromise for MognoClient connection
+*/
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -13,8 +20,8 @@ app.use(
       httpOnly: false,
       maxAge: parseInt(process.env.AUTH_AGE),
     },
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection,
+    store: new MongoStore.create({
+      clientPromise: clientPromise.then((m) => m.connection.getClient()),
       ttl: parseInt(process.env.AUTH_AGE) / 1000,
     }),
   })
