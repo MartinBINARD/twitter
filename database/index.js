55ad56ed37c9676d596f89f7918eebdd626c7a48
MartinBINARD <martin.binard.dev@gmail.com>
2023-11-02 10:04:41 +0100

    Update packages and remove depracted userNewUrlParser

--- database/index.js
+++ database/index.js
@@ -5,10 +5,7 @@ const mongoose = require("mongoose");
 
 exports.clientPromise = mongoose
   .connect(
-    `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.yxs0hv5.mongodb.net/twitter?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-    }
+    `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.yxs0hv5.mongodb.net/twitter?retryWrites=true&w=majority`
   )
   .then((client) => {
     console.log("connexion db ok !");
